92e00018000fa69a3bf821ba87214578a16a7473
edemkemalov <denlucky5445@gmail.com>
2024-06-20 18:29:05 +0500

    fixed path

--- publication/urls.py
+++ publication/urls.py
@@ -1,6 +1,2 @@
 from django.urls import path
-from .views import SendMessageView
 
-urlpatterns = [
-    path('send-message/', SendMessageView.as_view(), name='send_message'),
-]
--- publication/views.py
+++ publication/views.py
@@ -3,12 +3,3 @@ from django.views import View
 from .functions import send_telegram_message
 
 
-class SendMessageView(View):
-    def get(self, request):
-        token = '5463745047:AAFf0BgFRYCFIrIEsRhXWZnDTucBbI2VEPs'  # Store your token in Django settings
-        channel_username = '@test_23q21424'  # Store your channel username in settings
-        message = "Hello, this is a message from my Django app!"
-
-        response = send_telegram_message(token, channel_username, message)
-
-        return JsonResponse(response)
